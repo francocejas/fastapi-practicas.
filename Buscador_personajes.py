2323cea782d9cfa38b9ffdab26855e654f86b484
Franco Cejas <franco.cejas@itscordoba.edu.ar>
2026-05-05 17:05:48 -0300

    feat:buscador practica

---
+++ Buscador_personajes.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app= FastAPI()
+
+personajes=[{"nombre": "Harry Potter"}, {"nombre": "Albus Dumbledore"}, {"nombre":"Severus Snape"}, {"nombre": "Giny Weasley"}]
+
+@app.get("/personaje/{id}")
+def encontrar_personajes(id: int):
+    if id <0 or id >=len(personajes):  #el id debe ser menor que el largo de la lista
+           return{"error": "personaje no existe"} #método para no recorrer va directo a la posicion del objeto del diciconario
+    else:
+          return{"datos": personajes[id]}
